c31ea5dfe43ef15e38b757afc120971d1315888d
amina <elhijeamina42@gmail.com>
2024-09-21 00:00:31 +0100

    Ajout de la minification et mise à jour de package.json

---
+++ src/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Votre point d'entrée
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Dossier de sortie
+  },
+  mode: 'production', // Utiliser le mode production pour la minification
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
